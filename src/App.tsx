99d40eeaf261379588eb5e3959fe1af8710418f7
Mihir Vyas <mihirv50@gmail.com>
2025-03-18 11:54:34 +0530

    Animation Presence and layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,45 @@
 // import Drag from "./Components/Drag";
 // import SequencingAndStaggering from "./Components/SequencingAndStaggering";
 
-import { motion, useScroll } from "motion/react";
-import ScrollAnimations from "./Components/ScrollAnimations";
+import { AnimatePresence, motion, useScroll } from "motion/react";
+import { useState } from "react";
+// import ScrollAnimations from "./Components/ScrollAnimations";
 
 const App = () => {
-  const {scrollYProgress} = useScroll();
+  // const {scrollYProgress} = useScroll();
+  const [btnClicked, setBtnClicked] = useState<Boolean>(false);
+  const btnClicking: () => void = () => {
+    setBtnClicked(!btnClicked);
+    console.log(btnClicked);
+  };
   return (
     <>
-      <motion.div style={{scaleX:scrollYProgress}} className="w-full h-1 rounded-full bg-green-500 fixed origin-left"></motion.div>
-      <div className="w-screen min-h-screen bg-[#111] text-white px-80 p-20 text-center">
+      {/* <motion.div style={{scaleX:scrollYProgress}} className="w-full h-1 rounded-full bg-green-500 fixed origin-left"></motion.div> */}
+      <div className="w-screen min-h-screen bg-[#111] text-white p-10">
         {/* <Animation1/> */}
         {/* <Animation2/> */}
         {/* <Drag/> */}
         {/* <SequencingAndStaggering/> */}
-        <div className="bg-[#111]">
+        {/* <div className="bg-[#111]">
           <ScrollAnimations />
           <ScrollAnimations />
           <ScrollAnimations />
           <ScrollAnimations />
-        </div>
+        </div> */}
+        <AnimatePresence>
+          <motion.div
+            layout
+            className={`${btnClicked ? "h-[500px]" : "h-[200px]"} ${
+              btnClicked ? "w-[1000px]" : "w-[400px]"
+            } rounded-2xl bg-red-600`}
+          ></motion.div>
+          <button
+            onClick={() => btnClicking()}
+            className={`bg-emerald-500 mt-4 active:scale-95 font-bold rounded-xl px-6 py-2`}
+          >
+            Click
+          </button>
+        </AnimatePresence>
       </div>
     </>
   );
